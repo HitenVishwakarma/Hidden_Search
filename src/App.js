94dd38af3782cdeec7b07a80a9212ddd171a8927
Hiten Vishwakarma <hiten997764@gmail.com>
2022-05-12 22:23:34 +0530

    Implemented search button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const searchHandler = () => {
+    if (!isActive) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
   return (
     <div>
       <h1>Hidden Search</h1>
-      <div className="search">
+      <div className={`${isActive ? "search active" : "search"}`}>
         <input className="input" type="text" placeholder="Type to search..." />
-        <button className="btn">
+        <button className="btn" onClick={searchHandler}>
           <FontAwesomeIcon icon={faSearch} />
         </button>
       </div>
